e9355afa959e0340b37463e8ae55a5d2c75c9a2d
Ajay <ajayr5096@gmail.com>
2025-12-02 18:26:31 +0530

    first-commit

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -24,7 +24,13 @@ export const Home=()=>{
     hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
    >View Projects</a>
 
-    <a href="#contact"
+    <a href="https://drive.google.com/file/d/1PVGVaVcXXPACFo6fk9rZkrNoaQ-8qxqw/view?usp=sharing"
+    className=" border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
+    hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)]
+    hover:bg-blue-500/50"
+   >Download CV</a>
+
+   <a href="#contact"
     className=" border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
     hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)]
     hover:bg-blue-500/50"

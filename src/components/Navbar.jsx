d700206cbaaeba8466f616e17ef90eaef77b0e30
Ajay <ajayr5096@gmail.com>
2025-12-02 18:21:28 +0530

    first-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,16 +18,16 @@ export const Navbar=({menuOpen,setMenuOpen})=>{
     </div>
 
     <div className="hidden md:flex items-center space-x-8">
-        <a href="#home" className="text-gray-300 hover:tetx-white transition-colors">
+        <a href="#home" className="text-gray-300 hover:text-white transition-colors">
             Home
             </a>
-         <a href="#about" className="text-gray-300 hover:tetx-white transition-colors">
+         <a href="#about" className="text-gray-300 hover:text-white transition-colors">
             About
             </a>
-             <a href="#projects" className="text-gray-300 hover:tetx-white transition-colors">
+             <a href="#projects" className="text-gray-300 hover:text-white transition-colors">
             Projects
             </a>
-             <a href="#contact" className="text-gray-300 hover:tetx-white transition-colors">
+             <a href="#contact" className="text-gray-300 hover:text-white transition-colors">
             Contact
             </a>
 
